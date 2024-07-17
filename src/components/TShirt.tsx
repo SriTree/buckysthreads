364385ba6f7b35dc43d85baca5b80eab71b1e5a0
SriTree <kssrivatsaan@gmail.com>
2024-07-17 00:33:50 -0400

    fix 3

--- src/components/TShirt.tsx
+++ src/components/TShirt.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { HTMLAttributes, CSSProperties } from "react";
+import Image from "next/image";
 
 interface TShirtProps extends HTMLAttributes<HTMLDivElement> {
   imgSrc: string;
@@ -24,17 +25,22 @@ const TShirt = ({
       {...props}
     >
       <div className="relative z-10 w-full h-full">
-        <img
+        <Image
           src={dark ? "/shirt-template-light.png" : "/shirt-template-dark.png"}
           className="pointer-events-none select-none w-full h-full object-cover"
           alt="t-shirt image"
+          layout="fill"
+          objectFit="cover"
         />
       </div>
       <div className="absolute inset-0 z-20 flex items-center justify-center p-10 md:p-20 lg:p-24">
-        <img
-          className="object-contain w-[45%] h-[45%]"
+        <Image
+          className="object-contain"
           src={imgSrc}
           alt="overlaying t-shirt image"
+          layout="intrinsic"
+          width={500}
+          height={500}
         />
       </div>
     </div>
